ead1738c9c14d5aa6fba68009e0cae5c03f37bd4
Tommy <tommy.nguyen.13810@gmail.com>
2021-08-26 19:31:03 -0700

    added post method to post user name and dob

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 $("#getuser").on("click", async (e) => {
     try{
-        const user = await fetch('http://localhost:3000/getuser', {
+        const user = await fetch('http://localhost:3000/user', {
             method: 'GET',
         })
         const userdata = await user.json()
