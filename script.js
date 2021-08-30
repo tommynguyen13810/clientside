c62328b1d2bc68075afc9d8cb8656399776808bb
Tommy <tommy.nguyen.13810@gmail.com>
2021-08-30 12:46:23 -0700

    created getallusers and deleteallusers function

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 
 $("#getuser").on("click", async (e) => {
     try{
-        const user = await fetch('http://localhost:3000/user', {
+        const user = await fetch('http://localhost:3000/user/all', {
             method: 'GET',
         })
         const userdata = await user.json()
-        $("body").append(`<p>The user is ${userdata.name}<p>`)
+        console.log(userdata)
+        for(let i = 0; i < userdata.length; i++) {
+            $("body").append(`<p>The user is ${userdata[i].name} with dob ${userdata[i].dob}<p>`)
+        }
     }
     catch(e) {
         console.log("Error getting user")
+        console.log(e)
+    }
+})
+
+$("#deleteall").on("click", async (e) => {
+    try{
+        const user = await fetch('http://localhost:3000/user/deleteall', {
+            method: 'DELETE',
+        })
+        const userdata = await user.json()
+        console.log(userdata)
+    }
+    catch(e) {
+        console.log("Error deleting users")
         console.log(e)
     }
 })
