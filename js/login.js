8426029f56f88ad501bcbca030212e1f5a5ffe39
Tommy Nguyen <tommynguyen@169-231-98-120.wireless.ucsb.edu>
2021-10-06 22:47:32 -0700

    moved index.html to root directory

--- js/login.js
+++ js/login.js
@@ -1,74 +1,67 @@
 
 
 $(function () {
-    
+
     var emailRegex = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
     var strongPwdRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-_!@#\$%\^&\*])(?=.{8,})");
-    
+
     // To enable or disable login button
     $("#signinForm").bind('keyup', function (e) {
-        
-        var email=$("#email").val();
-        var password=$("#password").val();
-        
-        if(emailRegex.test(email)==true && strongPwdRegex.test(password)==true)
-        {
-            $("#signin").attr('disabled',false);
+
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        if (emailRegex.test(email) == true && strongPwdRegex.test(password) == true) {
+            $("#signin").attr('disabled', false);
         }
-        else
-        {
-            $("#signin").attr('disabled',true);
+        else {
+            $("#signin").attr('disabled', true);
         }
     });
-    
-    
+
+
     // To validate email entry
     $("#email").on("keyup", function (e) {
-        
-        var email=$("#email").val();
-        
-        if(email.length==0){
+
+        var email = $("#email").val();
+
+        if (email.length == 0) {
             $("#email_error").text("");
         }
-        else if(emailRegex.test(email)==false)
-        {
+        else if (emailRegex.test(email) == false) {
             $("#email_error").text("Invalid email");
         }
-        else
-        {
+        else {
             $("#email_error").text("");
-        }             
+        }
     });
-    
-    
+
+
     // To validate password entry
     $("#password").on("keyup", function (e) {
-        
-        var passwordValue=$("#password").val();
-        
-        if(passwordValue.length==0){
+
+        var passwordValue = $("#password").val();
+
+        if (passwordValue.length == 0) {
             $("#passsword_error").text("");
         }
-        else if(passwordValue.length<8)
-        {
+        else if (passwordValue.length < 8) {
             $("#passsword_error").text("8 characters at least");
         }
-        else if(strongPwdRegex.test(passwordValue)==false)
-        {
+        else if (strongPwdRegex.test(passwordValue) == false) {
             $("#passsword_error").text("Unsecured password");
         }
-        else
-        {
+        else {
             $("#passsword_error").text("");
-        }             
+        }
     });
-    
-    
+
+
 });
 
 //sign in button, checks if user exists and pass is correct, then redirects to user page
 $("#signin").on("click", async (e) => {
-    try{
+    try {
         console.log("triggered")
         const data = {
             email: `${$("#email").val()}`,
@@ -84,21 +77,21 @@ $("#signin").on("click", async (e) => {
         })
         const datajson = await newUser.json()
         console.log(datajson)
-        if(datajson == "User does not exist") {
+        if (datajson == "User does not exist") {
             $("#passsword_error").text("User Does Not Exist Or Info Is Incorrect");
         }
         else {
             storage = window.localStorage
             storage.setItem('id', datajson._id)
-            window.location = '../views/user.html'
+            window.location = './views/user.html'
         }
     }
-    catch(e) {
+    catch (e) {
         console.log("Error creating user")
         console.log(e)
     }
 })
 
-$("#signinForm").submit(function(e) {
+$("#signinForm").submit(function (e) {
     e.preventDefault();
 });
